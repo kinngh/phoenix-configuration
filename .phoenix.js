d443636d3d45f54d0082e3f2754e93d682cc08af
Harshdeep Singh Hura <kinngh@me.com>
2023-11-06 14:02:38 +0530

    🔨 Config

---
+++ .phoenix.js
@@ -0,0 +1,66 @@
+/**
+ *
+ * Thirds
+ *
+ */
+
+// Left third
+Key.on("left", ["ctrl", "alt", "cmd"], function () {
+  const screen = Screen.main().flippedVisibleFrame();
+  const window = Window.focused();
+  if (window) {
+    window.setFrame({
+      x: screen.x,
+      y: screen.y,
+      width: screen.width / 3,
+      height: screen.height,
+    });
+  }
+});
+
+// Middle third
+Key.on("down", ["ctrl", "alt", "cmd"], function () {
+  const screen = Screen.main().flippedVisibleFrame();
+  const window = Window.focused();
+  if (window) {
+    window.setFrame({
+      x: screen.x + screen.width / 3,
+      y: screen.y,
+      width: screen.width / 3,
+      height: screen.height,
+    });
+  }
+});
+
+// Right third
+Key.on("right", ["ctrl", "alt", "cmd"], function () {
+  const screen = Screen.main().flippedVisibleFrame();
+  const window = Window.focused();
+  if (window) {
+    window.setFrame({
+      x: screen.x + (screen.width * 2) / 3,
+      y: screen.y,
+      width: screen.width / 3,
+      height: screen.height,
+    });
+  }
+});
+
+/**
+ *
+ * Full Screen
+ *
+ */
+
+Key.on("up", ["ctrl", "alt", "cmd"], function () {
+  const screen = Screen.main().flippedVisibleFrame();
+  const window = Window.focused();
+  if (window) {
+    window.setFrame({
+      x: screen.x,
+      y: screen.y,
+      width: screen.width,
+      height: screen.height,
+    });
+  }
+});
